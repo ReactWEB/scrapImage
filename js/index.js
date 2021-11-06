198b4a842f5487c2f3f40db0f0de9da70127784b
ReactWEB <reactweb@ukr.net>
2021-11-06 09:14:16 +0200

    taking links from scrap html

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
-const puppeteer = require('puppeteer');
+let fs = require('fs');
+const request = require('request');
+const url = 'https://dou.ua';
 
-const start = (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://dou.ua');
-  
-  await page.waitForSelector('body > div.g-page > div.b-footer-slider.m-hide > div:nth-child(2)')
-  const subs = await page.$eval('body > div.g-page > div.b-footer-slider.m-hide > div:nth-child(2)', (el) => el.innerText);
-  console.log(subs);
+request(url).pipe(
+    fs.createWriteStream('dou-page.html')
+);
 
-  await browser.close();
+request(url, function(error, response, body) {
+  console.error('error:', error);
+  console.log('statusCode:', response && response.statusCode); 
+
+  const getHTML = (HTMLbody) => {
+    const startStr = HTMLbody.indexOf('<h3>Радимо почитати');
+    const endStr = HTMLbody.indexOf('<footer class="b-footer">');
+    console.log(startStr, endStr);
+    let str = '';
+    for (let i = startStr; i < endStr; i++){      
+        str = `${str}${HTMLbody[i]}`;
+    }
+    console.log(str);
+  };
+  getHTML(body);
 });
 
-start();
